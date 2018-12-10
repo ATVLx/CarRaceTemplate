a2be99280f0e9ec8f8fb70625d840256612b27cd
Vishal-iMac <vlxmenblack@gmail.com>
2018-12-10 11:17:45 +0530

    uploaded from Mac

---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof (CarController))]
+    public class CarUserControl : MonoBehaviour
+    {
+        public AxisTouchButton axisaccl;
+        public AxisTouchButton axisaccl2;
+        
+       //public Onbreak brek;
+        
+        private CarController m_Car; // the car controller we want to use
+
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            // pass the input to the car!
+    
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            /*  float b = axisaccl.Onbreak();
+               float v = axisaccl.Onaccelrate();*/
+            /*float b = -axisaccl2.Onaccelrate();
+            float v = axisaccl.Onaccelrate();*/
+            float b = -PlayerPrefs.GetFloat("accl");
+            float v = PlayerPrefs.GetFloat("accl");
+           
+#if !MOBILE_INPUT
+            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+            m_Car.Move(h,v, b, handbrake);
+#else
+            m_Car.Move(h,v, b, 0f);
+#endif
+        }
+    }
+}
---
+++ Assets/TurnTheGameOn/Racing Game Template/Scripts/OnBreak.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OnBreak : MonoBehaviour,IPointerDownHandler, IPointerUpHandler {
+    bool pointerdown = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+// Update is called once per frame
+void Update()
+{
+
+}
+    public void OnPointerDown(PointerEventData data)
+    {
+        pointerdown = true;
+    }
+    public void OnPointerUp(PointerEventData data)
+    {
+        pointerdown = false;
+    }
+
+    public float breeak(){
+        float brak;
+        if (pointerdown == true) {
+            brak = -1;
+        }
+        else {
+            brak = 0;
+
+        }
+        return brak;
+    }
+}
